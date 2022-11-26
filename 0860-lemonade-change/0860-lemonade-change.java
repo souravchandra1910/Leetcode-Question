3543f18e015438a6dc3016502b5fc82a953f97ab
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2022-11-26 12:55:41 +0530

    Time: 9 ms (36.36%), Space: 76.9 MB (23.73%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    public boolean lemonadeChange(int[] arr) {
+        int c5 = 0;
+        int c10 = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 5) {
+                c5++;
+            } else if (arr[i] == 10) {
+                if (c5 >= 1) {
+                    c5--;
+                    c10++;
+                } else {
+                    return false;
+                }
+            } else {
+                if (c5 >= 1 && c10 >= 1) {
+                    c5--;
+                    c10--;
+                } else if (c5 >= 3) {
+                    c5 -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
