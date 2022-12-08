41cc05c5b5e86581aeab2cddff9ef1b09629f622
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2022-12-08 19:15:11 +0530

    Added solution - LeetHub

---
+++ The Celebrity Problem - GFG/the-celebrity-problem.java
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*; 
+
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t>0)
+        {
+            int N = sc.nextInt();
+            int M[][] = new int[N][N];
+            for(int i=0; i<N; i++)
+            {
+                for(int j=0; j<N; j++)
+                {
+                    M[i][j] = sc.nextInt();
+                }
+            }
+            System.out.println(new Solution().celebrity(M,N));
+            t--;
+        }
+    } 
+} 
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution
+{ 
+    //Function to find if there is a celebrity in the party or not.
+    int celebrity(int [][]arr, int n)
+    {
+        Stack<Integer>st=new Stack<>();
+        for(int i=0;i<n;i++){
+            st.push(i);
+        }
+        while(st.size()>=2){
+            int i=st.pop();
+            int j=st.pop();
+            
+            if(arr[i][j]==1){
+                //  i knows j
+                st.push(j);
+            }else{
+                //i doesn't know j
+                st.push(i);
+            }
+        }
+        int pot=st.pop();
+        for(int i=0;i<n;i++){
+            if(i!=pot){
+                 if(arr[i][pot]==0 || arr[pot][i]==1){
+                     return -1;
+                 }
+            }
+        }
+        return pot;
+        
+    }
+}
