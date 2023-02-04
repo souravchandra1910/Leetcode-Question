4320ddf00a67eb90e4a2dc97635f0d1bcbc27eb8
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2023-02-04 19:16:17 +0530

    Time: 2 ms (66.63%), Space: 62.2 MB (78.47%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,23 +1,21 @@
 class Solution {
-
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        // 1st step check if the gas provided to us is > what it takes to make the distance
-
-        int sum = 0;
-        for (int i = 0; i < gas.length; i++) {
-            sum += gas[i];
-            sum -= cost[i];
+        //1st step check whether that total gas jo hai wo total cost se kam to nahi
+        int tgas=0,tcost=0;
+        for(int i=0;i<gas.length;i++){
+            tgas+=gas[i];
+            tcost+=cost[i];
         }
-        if (sum < 0) return -1;
-
-        //step 2 check from where cost if not -ve and start from there
-        int rem = 0;
-        int ans = 0;
-        for (int i = 0; i < gas.length - 1; i++) {
-            rem += (gas[i] - cost[i]);
-            if (rem < 0) {
-                ans = i + 1;
-                rem = 0;
+        if(tgas<tcost)return -1;
+        
+        tgas=0;
+        int ans=0;
+        for(int i=0;i<gas.length-1;i++){
+            tgas+=gas[i];
+            tgas-=cost[i];
+            if(tgas<0){
+                ans=i+1;
+                tgas=0;
             }
         }
         return ans;
