17231d29a818f73e7a5000370922f0697ea04921
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2023-03-08 20:19:25 +0530

    Time: 87 ms (52.46%), Space: 45.1 MB (51.58%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} piles
+ * @param {number} h
+ * @return {number}
+ */
+var minEatingSpeed = function(piles, h) {
+    let low=1,high=0;
+    for(let i=0;i<piles.length;i++)high+=piles[i];
+    let ans=0;
+    while(low<=high){
+        let mid=Math.floor((low+high)/2)
+        if(blackBox(piles,mid,h)==true){
+            ans=mid;
+            high=mid-1;
+        }else{
+            low=mid+1;
+        }
+    }
+    return ans;
+};
+
+function blackBox(arr,mid,h){
+    let cnt=0;
+    for(let i=0;i<arr.length;i++){
+       cnt=cnt+Math.ceil(arr[i]/mid);
+        
+    }
+    return cnt<=h;
+}
