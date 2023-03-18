e66130fd3c54d5ecfb1c4b632d92b0d62b5331d8
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2023-03-18 19:53:26 +0530

    Added solution - LeetHub

--- Stickler Thief - GFG/stickler-thief.java
+++ Stickler Thief - GFG/stickler-thief.java
@@ -36,16 +36,16 @@ class Solution
     //Function to find the maximum money the thief can get.
     public int FindMaxSum(int arr[], int n)
     {
-        int []dp=new int[n];
-        dp[0]=arr[0];
+        int inc=arr[0];
+        int exc=0;
         for(int i=1;i<n;i++){
-            int pick=arr[i];
-            if(i>1)pick+=dp[i-2];
-            int notpick=0+dp[i-1];
-            dp[i]=Math.max(pick,notpick);
+            int ninc=exc+arr[i];
+            int nexc=Math.max(inc,exc);
             
+            inc=ninc;
+            exc=nexc;
         }
-        return dp[n-1];
+        return Math.max(inc,exc);
     }
 }
 // dp[0]=5
