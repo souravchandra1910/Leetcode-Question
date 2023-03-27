a63d8f0a3019cdcecaf0c491e01f3c9424bd5283
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2023-03-27 19:33:18 +0530

    Time: 3 ms (32.07%), Space: 44.8 MB (89.21%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,25 +1,26 @@
 class Solution {
 
     public int minPathSum(int[][] arr) {
-        int n = arr.length, m = arr[0].length;
+        int n = arr.length;
+        int m = arr[0].length;
         int[][] dp = new int[n][m];
         for (int i = n - 1; i >= 0; i--) {
             for (int j = m - 1; j >= 0; j--) {
-                // if we are at the last cell
+                //we are in last cell
                 if (i == n - 1 && j == m - 1) {
                     dp[i][j] = arr[i][j];
                 }
-                //if we are at the last row
-                else if (i == n - 1) {
-                    dp[i][j] = arr[i][j] + dp[i][j + 1];
-                }
-                //if we are at the last col
+                // we are in the last column
                 else if (j == m - 1) {
                     dp[i][j] = arr[i][j] + dp[i + 1][j];
                 }
+                // we are last row;
+                else if (i == n - 1) {
+                    dp[i][j] = arr[i][j] + dp[i][j + 1];
+                }
                 // else where
                 else {
-                    dp[i][j] = arr[i][j] + Math.min(dp[i][j + 1], dp[i + 1][j]);
+                    dp[i][j] = Math.min(dp[i + 1][j], dp[i][j + 1]) + arr[i][j];
                 }
             }
         }
