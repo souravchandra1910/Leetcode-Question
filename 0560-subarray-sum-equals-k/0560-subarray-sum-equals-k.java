bee45bc3ab5d5e3185bb6f226a48b688dd2c4b6d
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2023-04-06 20:06:42 +0530

    Time: 27 ms (46.01%), Space: 47.3 MB (19.14%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -2,18 +2,19 @@ class Solution {
 
     public int subarraySum(int[] nums, int k) {
         // idea is to keep on calculating prefix sum and store it in the map
-        // if map.contains(k-sum)==true
+        // if map.contains(sum-k)==true (if sum repeats)
         // cnt++;
 
         HashMap<Integer, Integer> hm = new HashMap<>();
-        int sum = 0, cnt = 0;
+        int tsum = 0, cnt = 0;
         hm.put(0, 1);
         for (int val : nums) {
-            sum += val;
-            if (hm.containsKey(sum - k) == true) {
-                cnt += hm.get(sum - k);
+            tsum += val;
+            int remaining_sum = tsum - k;
+            if (hm.containsKey(remaining_sum) == true) {
+                cnt += hm.get(remaining_sum);
             }
-            hm.put(sum, hm.getOrDefault(sum, 0) + 1);
+            hm.put(tsum, hm.getOrDefault(tsum, 0) + 1);
         }
         return cnt;
     }
