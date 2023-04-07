948077d4a03e0d29eb278a742bb3f2f36aa54d3e
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2023-04-07 16:59:19 +0530

    Time: 0 ms (100.00%), Space: 44.4 MB (22.24%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -15,7 +15,7 @@ class Solution {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (row[i] == 1 || col[j] == 1) {
-                    if (arr[i][j] != 0) arr[i][j] = 0;
+                    arr[i][j] = 0;
                 }
             }
         }
