40fb7f5e6c1c2ec0313a276dc7f31cac26a8ba1a
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2023-04-16 17:35:01 +0530

    Time: 2 ms (100.00%), Space: 42.4 MB (80.00%) - LeetHub

--- 2645-minimum-additions-to-make-valid-string/2645-minimum-additions-to-make-valid-string.java
+++ 2645-minimum-additions-to-make-valid-string/2645-minimum-additions-to-make-valid-string.java
@@ -1,27 +1,27 @@
 class Solution {
 
-    public int addMinimum(String str) {
+    public int addMinimum(String word) {
         int ans = 0;
-        char[] word = str.toCharArray();
-        int i = 0, n = word.length;
+        char[] str = word.toCharArray();
+        int i = 0, n = str.length;
         while (i < n) {
-            if (word[i] == 'a') {
-                if (i + 1 < n && word[i + 1] == 'b') {
-                    if (i + 2 < n && word[i + 2] == 'c') {
+            if (str[i] == 'a') { // character at ith idx can either be a or b or c
+                if (i + 1 < n && str[i + 1] == 'b') {
+                    if (i + 2 < n && str[i + 2] == 'c') {
                         i += 3; //abc are in sequence
                     } else {
                         i += 2;
                         ans++; // ab are sequence and only c is needed to be added
                     }
-                } else if (i + 1 < n && word[i + 1] == 'c') { // a,c are forming a sequence so b is to append in middle
+                } else if (i + 1 < n && str[i + 1] == 'c') { // a,c are forming a sequence so b is to append in middle
                     i += 2;
                     ans += 1;
                 } else { // a comes after a i.e aa
                     i++;
                     ans += 2;
                 }
-            } else if (word[i] == 'b') {
-                if (i + 1 < n && word[i + 1] == 'c') { //bc is the sequence a is to added
+            } else if (str[i] == 'b') {
+                if (i + 1 < n && str[i + 1] == 'c') { //bc is the sequence a is to added
                     i += 2;
                     ans += 1;
                 } else {
