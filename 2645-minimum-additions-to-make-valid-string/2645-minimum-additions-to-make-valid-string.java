1bf4d43403c7e7d0d2382f62fd3efaceb68c3762
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2023-04-17 08:11:33 +0530

    Time: 3 ms (100.00%), Space: 42.4 MB (80.00%) - LeetHub

--- 2645-minimum-additions-to-make-valid-string/2645-minimum-additions-to-make-valid-string.java
+++ 2645-minimum-additions-to-make-valid-string/2645-minimum-additions-to-make-valid-string.java
@@ -1,35 +1,19 @@
 class Solution {
 
     public int addMinimum(String word) {
-        int ans = 0;
-        char[] str = word.toCharArray();
-        int i = 0, n = str.length;
-        while (i < n) {
-            if (str[i] == 'a') { // character at ith idx can either be a or b or c
-                if (i + 1 < n && str[i + 1] == 'b') {
-                    if (i + 2 < n && str[i + 2] == 'c') {
-                        i += 3; //abc are in sequence
-                    } else {
-                        i += 2;
-                        ans++; // ab are sequence and only c is needed to be added
-                    }
-                } else if (i + 1 < n && str[i + 1] == 'c') { // a,c are forming a sequence so b is to append in middle
-                    i += 2;
-                    ans += 1;
-                } else { // a comes after a i.e aa
-                    i++;
-                    ans += 2;
-                }
-            } else if (str[i] == 'b') {
-                if (i + 1 < n && str[i + 1] == 'c') { //bc is the sequence a is to added
-                    i += 2;
-                    ans += 1;
-                } else {
-                    i++; // b comes after b
-                    ans += 2;
-                }
+        int n = word.length(), ans = 0;
+
+        for (int i = 0; i < n;) {
+            String s = "";
+            if (i + 1 < n) s = word.substring(i, i + 2); else s = word.charAt(i) + "";
+
+            if (i + 2 < n && word.substring(i, i + 3).equals("abc")) {
+                i += 3;
+            } else if (s.equals("ab") || s.equals("bc") || s.equals("ac")) {
+                ans += 1;
+                i += 2;
             } else {
-                i++; // c is sequence and a,b are to be added
+                i++;
                 ans += 2;
             }
         }
