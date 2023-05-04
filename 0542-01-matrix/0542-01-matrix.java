1fad5557b05721e8758cf6123c3556d7bdead1c4
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2023-05-04 08:39:33 +0530

    Time: 18 ms (23.89%), Space: 45.2 MB (75.82%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.java
@@ -0,0 +1,46 @@
+class Solution {
+    int[] dx = { -1, 0, 0, 1 };
+    int[] dy = { 0, -1, 1, 0 };
+
+    class Pair {
+        int row, col, dis;
+
+        Pair(int row, int col, int dis) {
+            this.row = row;
+            this.col = col;
+            this.dis = dis;
+        }
+    }
+
+    public int[][] updateMatrix(int[][] mat) {
+        int n = mat.length, m = mat[0].length;
+        int[][] ans = new int[n][m];
+        boolean[][] vis = new boolean[n][m];
+        Queue<Pair> mq = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] == 0) {
+                    vis[i][j] = true;
+                    mq.add(new Pair(i, j, 0));
+                }
+            }
+        }
+
+        while (mq.size() > 0) {
+            Pair p = mq.poll();
+            int row = p.row;
+            int col = p.col;
+            int dis = p.dis;
+            ans[row][col] = dis;
+            for (int i = 0; i < 4; i++) {
+                int nrow = row + dx[i];
+                int ncol = col + dy[i];
+                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && vis[nrow][ncol] == false && mat[nrow][ncol] == 1) {
+                    vis[nrow][ncol] = true;
+                    mq.add(new Pair(nrow, ncol, dis+1));
+                }
+            }
+        }
+        return ans;
+    }
+}
