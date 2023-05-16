e9d9a548ac693608d07ed9f1ebeddc20ff6b3bf1
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2023-05-16 08:22:46 +0530

    Time: 35 ms (93.15%), Space: 59.6 MB (54.31%) - LeetHub

--- 2653-sliding-subarray-beauty/2653-sliding-subarray-beauty.java
+++ 2653-sliding-subarray-beauty/2653-sliding-subarray-beauty.java
@@ -1,38 +1,31 @@
 class Solution {
 
     public int[] getSubarrayBeauty(int[] nums, int k, int x) {
-        int n = nums.length;
-        int[] ans = new int[n - k + 1];
-        int idx = 0;
         int[] map = new int[50];
-        int j = 0;
+        int j = 0, idx = 0;
+        int n = nums.length;
+
+        int[] ans = new int[n - k + 1];
         for (int i = 0; i < n; i++) {
             if (nums[i] < 0) {
-                int val = nums[i] + 50;
-                map[val]++;
+                map[nums[i] + 50]++;
             }
             if (i - j + 1 == k) {
                 int cnt = 0;
-                boolean flag = true;
+                boolean flag = false;
                 for (int a = 0; a < 50; a++) {
-                    if (map[a] != 0) {
-                        if (map[a] + cnt >= x) {
-                            ans[idx++] = a - 50;
-                            flag = false;
-                            cnt = 0;
-                            break;
-                        } else {
-                            cnt += map[a];
-                        }
+                    if (cnt + map[a] >= x) {
+                        ans[idx++] = a - 50;
+                        cnt = 0;
+                        flag = true;
+                        break;
+                    } else {
+                        cnt += map[a];
                     }
                 }
-                if (flag) {
-                    ans[idx++] = 0;
-                    cnt = 0;
-                }
+                if (flag == false) ans[idx++] = 0;
                 if (nums[j] < 0) {
-                    int val = nums[j] + 50;
-                    map[val]--;
+                    map[nums[j] + 50]--;
                 }
                 j++;
             }
