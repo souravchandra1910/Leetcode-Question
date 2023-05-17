2d2530f3e41ff7c076f69d08b3e860329b295bbf
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2023-05-17 08:58:51 +0530

    Time: 19 ms (34.82%), Space: 52.1 MB (64.18%) - LeetHub

--- 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.java
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.java
@@ -1,29 +1,49 @@
 class Solution {
 
-    public int maxMoves(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
+    public int maxMoves(int[][] arr) {
+        int n = arr.length;
+        int m = arr[0].length;
         int[][] dp = new int[n][m];
         for (int j = m - 2; j >= 0; j--) {
-            for (int i = 0; i < n; i++) {
+            for (int i = n - 1; i >= 0; i--) {
                 if (i == 0) {
-                    if (grid[i][j + 1] > grid[i][j]) dp[i][j] = Math.max(dp[i][j], 1 + dp[i][j + 1]);
-                    if (grid[i + 1][j + 1] > grid[i][j]) dp[i][j] = Math.max(dp[i][j], 1 + dp[i + 1][j + 1]);
+                    int max=-1;
+                    if (arr[i + 1][j + 1] > arr[i][j]) {
+                        max = Math.max(dp[i + 1][j + 1], max);
+                    }
+                    if (arr[i][j + 1] > arr[i][j]) {
+                        max = Math.max(dp[i][j + 1], max);
+                    }
+                    dp[i][j]=max==-1?0:max + 1;
                 } else if (i == n - 1) {
-                    if (grid[i - 1][j + 1] > grid[i][j]) dp[i][j] = Math.max(dp[i][j], 1 + dp[i - 1][j + 1]);
-                    if (grid[i][j + 1] > grid[i][j]) dp[i][j] = Math.max(dp[i][j], 1 + dp[i][j + 1]);
+                    int max = -1;
+                    if (arr[i - 1][j + 1] > arr[i][j]) {
+                        max = Math.max(dp[i - 1][j + 1], max);
+                    }
+                    if (arr[i][j + 1] > arr[i][j]) {
+                        max = Math.max(dp[i][j + 1], max);
+                    }
+                    dp[i][j]=max==-1?0:max + 1;
                 } else {
-                    if (grid[i - 1][j + 1] > grid[i][j]) dp[i][j] = Math.max(dp[i][j], 1 + dp[i - 1][j + 1]);
-                    if (grid[i][j + 1] > grid[i][j]) dp[i][j] = Math.max(dp[i][j], 1 + dp[i][j + 1]);
-                    if (grid[i + 1][j + 1] > grid[i][j]) dp[i][j] = Math.max(dp[i][j], 1 + dp[i + 1][j + 1]);
+                    int max = -1;
+                    if (arr[i - 1][j + 1] > arr[i][j]) {
+                        max = Math.max(dp[i - 1][j + 1], max);
+                    }
+                    if (arr[i + 1][j + 1] > arr[i][j]) {
+                        max = Math.max(dp[i + 1][j + 1], max);
+                    }
+                    if (arr[i][j + 1] > arr[i][j]) {
+                        max = Math.max(dp[i][j + 1], max);
+                    }
+                    dp[i][j]=max==-1?0:max + 1;
                 }
             }
         }
-         for (int[] a : dp) System.out.println(Arrays.toString(a));
-        int ans = 0;
+        // for (int[] a : dp) System.out.println(Arrays.toString(a));
+        int max = 0;
         for (int i = 0; i < n; i++) {
-            ans = Math.max(ans, dp[i][0]);
+            max = Math.max(max, dp[i][0]);
         }
-        return ans;
+        return max;
     }
 }
