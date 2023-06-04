6406b1832cef6642070a83ddba98173ae84cc8d4
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2023-06-04 21:31:41 +0530

    Time: 3 ms (34.77%), Space: 44.6 MB (7.22%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -3,16 +3,15 @@ class Solution {
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
         ArrayList<Integer>[] adj = new ArrayList[n];
-        for(int i=0;i<n;i++)adj[i]=new ArrayList<>();
+        for (int i = 0; i < n; i++) adj[i] = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (isConnected[i][j] == 1 && i != j) { // i!=j is to for checking self loop there must not exist a self loop
+                if (isConnected[i][j] == 1 && i != j) { // i!=j is to for checking self loop there                     must not exist a self loop
                     adj[i].add(j);
                     adj[j].add(i);
                 }
             }
         }
-
         int cnt = 0;
         boolean[] vis = new boolean[n];
         for (int i = 0; i < n; i++) {
