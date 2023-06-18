8a430d48b7fd77a74d59a81095235311081748b2
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2023-06-18 17:43:03 +0530

    Added solution - LeetHub

--- Consecutive 1's not allowed - GFG/consecutive-1s-not-allowed.java
+++ Consecutive 1's not allowed - GFG/consecutive-1s-not-allowed.java
@@ -27,14 +27,25 @@ class GFG {
 class Solution {
     long mod=(long)1e9+7;
     long countStrings(int n) {
-         long []ones=new long[n+1];
-         long []zeros=new long[n+1];
-         ones[1]=1;
-         zeros[1]=1;
-         for(int i=2;i<=n;i++){
-             zeros[i]=(ones[i-1]%mod+zeros[i-1]%mod)%mod;
-             ones[i]=zeros[i-1]%mod;
-         }
-         return (ones[n]%mod+zeros[n]%mod)%mod;
+        //  long []ones=new long[n+1];
+        //  long []zeros=new long[n+1];
+        //  ones[1]=1;
+        //  zeros[1]=1;
+        //  for(int i=2;i<=n;i++){
+        //      zeros[i]=(ones[i-1]%mod+zeros[i-1]%mod)%mod;
+        //      ones[i]=zeros[i-1]%mod;
+        //  }
+        //  return (ones[n]%mod+zeros[n]%mod)%mod;
+        
+        long a=1;
+        long b=1;
+        for(int i=2;i<=n;i++){
+            long c=(a%mod+b%mod)%mod;
+            long d=a%mod;
+            
+            a=c;
+            b=d;
+        }
+        return (a%mod+b%mod)%mod;
     }
 }
