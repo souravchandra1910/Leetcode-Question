5ea66899ca6cd993201a0f75a91832ec017a5c9b
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2023-06-23 10:19:50 +0530

    Time: 1 ms (94.20%), Space: 44.9 MB (8.19%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -4,18 +4,15 @@ class Solution {
         int buy_day=0;
         int sell_day=0;
         int profit=0;
-        boolean flag=false;
         for(int i=1;i<n;i++){
             if(prices[i]>=prices[i-1]){
                 sell_day++;
-                flag=false;
             }else{
-                flag=true;
                 profit+=prices[sell_day]-prices[buy_day];
                 buy_day=sell_day=i;
             }
         }
-        if(flag==false)profit+=prices[sell_day]-prices[buy_day];
+        profit+=prices[sell_day]-prices[buy_day];
         return profit;
     }
 }
