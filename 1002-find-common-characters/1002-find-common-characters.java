209b8a9524c256be736ba19321afd3aa3c596369
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2023-07-04 07:29:00 +0530

    Time: 19 ms (28.91%), Space: 44.2 MB (17.45%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.java
+++ 1002-find-common-characters/1002-find-common-characters.java
@@ -1,28 +1,30 @@
 class Solution {
 
     public List<String> commonChars(String[] words) {
+        //1st create a map using 1st string
         HashMap<Character, Integer> hm = new HashMap<>();
         for (char ch : words[0].toCharArray()) {
             hm.put(ch, hm.getOrDefault(ch, 0) + 1);
         }
-        List<String>al=new ArrayList<>();
-        for(int i=1;i<words.length;i++){
-            String s=words[i];
-            HashMap<Character,Integer>hm1=new HashMap<>();
-            for(char ch:s.toCharArray()){
-                hm1.put(ch,hm1.getOrDefault(ch,0)+1);
+        //from 2nd string check what is not matching and remove it from the map
+        for (int i = 1; i < words.length; i++) {
+            String s = words[i];
+            HashMap<Character, Integer> hm1 = new HashMap<>();
+            for (char ch : s.toCharArray()) {
+                hm1.put(ch, hm1.getOrDefault(ch, 0) + 1);
             }
-            HashMap<Character,Integer>hm2=new HashMap<>();
-            for(char key:hm1.keySet()){
-                if(hm.containsKey(key)){
-                    hm2.put(key,Math.min(hm1.get(key),hm.get(key)));
+            HashMap<Character, Integer> temp = new HashMap<>();
+            for (char key : hm.keySet()) {
+                if (hm1.containsKey(key) == true) {
+                    temp.put(key, Math.min(hm.get(key), hm1.get(key)));
                 }
             }
-            hm=hm2;
+            hm = temp;
         }
-        for(char key:hm.keySet()){
-            int freq=hm.get(key);
-            while(freq-->0){
+        List<String> al = new ArrayList<>();
+        for (char key : hm.keySet()) {
+            int freq = hm.get(key);
+            while (freq--> 0) {
                 al.add(key+"");
             }
         }
