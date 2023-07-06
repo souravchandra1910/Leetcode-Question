5159f5714a387c1e23f4d343d09dbb03958589d0
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2023-07-06 08:06:52 +0530

    Time: 1 ms (100.00%), Space: 56.3 MB (7.83%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,31 +1,24 @@
 class Solution {
 
     public int minSubArrayLen(int target, int[] nums) {
-        int low = 1;
-        int high = nums.length;
-        int ans = 0;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (blackBox(nums, nums.length, mid, target) == true) {
-                ans = mid;
-                high = mid - 1;
-            }else {
-                low = mid + 1;
+        int sum=0;
+        int minLen=0;
+        int j=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            if(sum>=target){
+               while(sum>=target){
+                   int len=i-j+1;
+                   if(minLen==0 || len<minLen){
+                       minLen=len;
+                   }
+                   sum-=nums[j++];
+               } 
+            }else{
+                continue;
             }
         }
-        return ans;
-    }
-
-    private boolean blackBox(int[] nums, int n, int k, int target) {
-        int sum = 0;
-        for (int i = 0; i < k; i++) sum += nums[i];
-        if (sum >= target) return true;
-        int j = 0;
-        for (int i = k; i < n; i++) {
-            sum -= nums[j++];
-            sum += nums[i];
-            if (sum >= target) return true;
-        }
-        return false;
+        return minLen;
+   
     }
 }
