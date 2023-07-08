448a17ea4c1b2551ad08a49be0e073937d72bdd6
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2023-07-08 08:43:01 +0530

    Time: 207 ms (7.58%), Space: 57 MB (54.03%) - LeetHub

--- 2401-longest-nice-subarray/2401-longest-nice-subarray.java
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.java
@@ -2,39 +2,31 @@ class Solution {
 
     public int longestNiceSubarray(int[] nums) {
         int[] set = new int[31];
-
         int maxLen = 1;
-        int left = 0;
-        for (int right = 0; right < nums.length; right++) {
-            int val = nums[right];
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
             boolean flag = false;
-            for (int i = 0; i < 31; i++) {
-                if ((val & (1 << i)) != 0) {
-                    set[i]++;
-                    if (set[i] > 1) {
+            for (int k = 0; k < 31; k++) {
+                if ((nums[i] & (1 << k)) != 0) {
+                    set[k]++;
+                    if (set[k] > 1) {
                         flag = true;
                     }
                 }
             }
-            if (flag == false) {
-                maxLen = Math.max(maxLen, right - left + 1);
-            } else {
-                while (left <= right) {
-                    int v = nums[left];
-                    for (int i = 0; i < 31; i++) {
-                        if ((v & (1 << i)) != 0) {
-                            set[i]--;
+            if (flag == false) maxLen = Math.max(maxLen, i - j + 1); else {
+                while (j <= i) {
+                    for (int k = 0; k < 31; k++) {
+                        if ((nums[j] & (1<< k)) != 0) {
+                            set[k]--;
                         }
                     }
-                    left++;
+                    j++;
                     boolean f = false;
-                    for (int i = 0; i < 31; i++) {
-                        if (set[i] > 1) f = true;
+                    for (int k = 0; k < 31; k++) {
+                        if (set[k]> 1) f = true;
                     }
-                    if (f == false) {
-                        // flag = true;
-                        break;
-                    }
+                    if (f == false) break;
                 }
             }
         }
