8aac8ff7dbc0faa8d2ed08e52717e38a2197985d
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2023-07-16 09:32:26 +0530

    Time: 42 ms (34.30%), Space: 57.8 MB (13.98%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
@@ -1,26 +1,25 @@
 class Solution {
 
-    public int maxOperations(int[] arr, int k) {
+    public int maxOperations(int[] nums, int k) {
         HashMap<Integer, Integer> hm = new HashMap<>();
-        for (int i = 0; i < arr.length; i++) {
-            hm.put(arr[i], hm.getOrDefault(arr[i], 0) + 1);
+        for (int val : nums) {
+            hm.put(val, hm.getOrDefault(val, 0) + 1);
         }
-        int ans = 0;
-        for (int i = 0; i < arr.length; i++) {
-            int p1 = arr[i];
-            int p2 = k - arr[i];
-            if (hm.containsKey(p2) == true) {
+        int cnt = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int p1 = nums[i];
+            int p2 = k - p1;
+            if (hm.containsKey(p2)) {
                 if (p1 == p2) {
-                    ans += hm.get(p1) / 2;
+                    cnt += hm.get(p1) / 2;
                     hm.remove(p1);
                 } else {
-                    ans += Math.min(hm.get(p1), hm.get(p2));
-                    hm.remove(p2);
+                    cnt += Math.min(hm.get(p1), hm.get(p2));
                     hm.remove(p1);
+                    hm.remove(p2);
                 }
             }
         }
-        hm.clear();
-        return ans;
+        return cnt;
     }
 }
