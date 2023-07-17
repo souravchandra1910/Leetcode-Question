32f3660801122d8535cac6dd892084b2efb6229c
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2023-07-17 08:09:21 +0530

    Time: 45 ms (20.00%), Space: 56.9 MB (20.00%) - LeetHub

--- 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.java
+++ 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.java
@@ -1,31 +1,26 @@
 class Solution {
 
     public int minimumIndex(List<Integer> nums) {
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        int maxE = 0;
         int maxF = 0;
+        int maxE = 0;
+        HashMap<Integer, Integer> hm = new HashMap<>();
         for (int val : nums) {
-            int freq = hm.getOrDefault(val, 0) + 1;
+            hm.put(val, hm.getOrDefault(val, 0) + 1);
+            int freq = hm.get(val);
             if (freq > maxF) {
                 maxF = freq;
                 maxE = val;
             }
-            hm.put(val, freq);
         }
-        // System.out.println(maxE);
-        int n = nums.size();
         int cnt = 0;
         int idx = -1;
-        boolean f1 = false, f2 = false;
+        int n = nums.size();
         for (int i = 0; i < n; i++) {
             int val = nums.get(i);
-
             if (val == maxE) {
                 cnt++;
-                // System.out.println(cnt+" "+(maxF-cnt));
             }
-            if ((2 * cnt > i + 1) && ((2 * (maxF - cnt)) > (n - i - 1))) {
-                
+            if (2 * cnt > i + 1 && 2 * (maxF - cnt) > (n - i - 1)) {
                 idx = i;
                 break;
             }
