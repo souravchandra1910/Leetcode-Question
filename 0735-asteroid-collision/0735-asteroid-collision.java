e67968c271e0cda350a8a0d8a9cf9f148321aa48
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2023-07-22 21:39:05 +0530

    Time: 11 ms (62.22%), Space: 44.1 MB (60.08%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -3,18 +3,18 @@ class Solution {
     public int[] asteroidCollision(int[] arr) {
         Stack<Integer> st = new Stack<>();
         for (int val : arr) {
-            if (val > 0 || st.size() == 0) {
+            if (val > 0) {
                 st.push(val);
             } else {
                 while (st.size() > 0 && st.peek() > 0 && Math.abs(st.peek()) < Math.abs(val)) {
                     st.pop();
                 }
-                if (st.size() > 0 && val == -st.peek()) {
+                if (st.size() > 0 && st.peek() > -val) {
+                    
+                } else if (st.size() > 0 && val == -st.peek()) {
                     st.pop();
                 } else {
-                    if (st.size() == 0 || st.peek() < 0) {
-                        st.push(val);
-                    }
+                    st.push(val);
                 }
             }
         }
