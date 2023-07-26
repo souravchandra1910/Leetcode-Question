60adc58464f2a5a410771158dde0e8994f8bd59d
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2023-07-26 18:58:32 +0530

    Time: 2 ms (82.61%), Space: 45.2 MB (94.15%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
@@ -6,13 +6,9 @@ class Solution {
         ans[n - 1] = -1;
         int max = arr[n - 1];
         for (int i = n - 2; i >= 0; i--) {
-            int curr_val = arr[i];
             ans[i] = max;
-            if (curr_val >= max) {
-                max = curr_val;
-            }
+            max = Math.max(max, arr[i]);
         }
-
         return ans;
     }
 }
