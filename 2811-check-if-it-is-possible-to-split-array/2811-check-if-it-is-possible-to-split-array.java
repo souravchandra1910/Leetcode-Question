434c0c2c3a7d77c58a1352a15c40cc2cd3b5bb28
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2023-08-07 09:14:15 +0530

    Time: 1 ms (100.00%), Space: 42.5 MB (100.00%) - LeetHub

---
+++ 2811-check-if-it-is-possible-to-split-array/2811-check-if-it-is-possible-to-split-array.java
@@ -0,0 +1,7 @@
+class Solution {
+
+    public boolean canSplitArray(List<Integer> nums, int m) {
+        for (int i = 0; i < nums.size() - 1; i++) if (nums.get(i) + nums.get(i + 1) >= m) return true;
+        return nums.size() < 3;
+    }
+}
