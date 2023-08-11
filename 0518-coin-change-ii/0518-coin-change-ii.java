52148ec1dd223d9749c5e7cf7f0d594ef9e50e1f
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2023-08-11 08:48:59 +0530

    Time: 3 ms (91.90%), Space: 39.7 MB (98.05%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,10 +1,11 @@
 class Solution {
-    public int change(int amt ,int[] arr) {
-        int []dp=new int[amt+1];
-        dp[0]=1;
-        for(int i=0;i<arr.length;i++){
-            for(int j=arr[i];j<=amt;j++){
-                dp[j]+=dp[j-arr[i]];
+
+    public int change(int amt, int[] arr) {
+        int[] dp = new int[amt + 1];
+        dp[0] = 1;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = arr[i]; j < dp.length; j++) {
+                dp[j] += dp[j - arr[i]];
             }
         }
         return dp[amt];
