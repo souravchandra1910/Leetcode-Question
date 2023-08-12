dd765169b45f6920eec6de25e04b5e3ea56e9717
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2023-08-12 10:13:55 +0530

    Time: 52 ms (21.50%), Space: 54.6 MB (97.29%) - LeetHub

--- 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.java
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.java
@@ -11,17 +11,18 @@ class Solution {
         // 1)agar freq of x is abc then freq of k-x must also be abc
         // 2)if x==k-x hai then freq of must be even (x=k/2 and k-x=k/2)
         // 3)if rem==0 then freq must be even
-
         for (int val : arr) {
-            int x = ((val % k) + k) % k;
-            if (x == 0) {
-                if (hm.get(x) % 2 == 1) return false;
-            } else if (x * 2 == k) {
-                if (hm.get(x) % 2 == 1) return false;
+            int rem = ((val % k) + k) % k;
+            if (rem == 0) {
+                int freq = hm.get(rem);
+                if (freq % 2 == 1) return false;
+            } else if (rem * 2 == k) {
+                int freq = hm.get(rem);
+                if (freq % 2 == 1) return false;
             } else {
-                int f1=hm.get(x);
-                int f2=hm.getOrDefault(k-x,0);
-                if(f1!=f2)return false;
+                int freq = hm.get(rem);
+                int ofreq = hm.getOrDefault(k - rem, 0);
+                if (freq != ofreq) return false;
             }
         }
         return true;
