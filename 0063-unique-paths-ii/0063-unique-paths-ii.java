3ca7154500f00cff8855dc7e3189b7d9b0dcf313
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2023-08-12 10:02:29 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (82.27%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,31 +1,25 @@
 class Solution {
 
     public int uniquePathsWithObstacles(int[][] arr) {
-        int n = arr.length, m = arr[0].length;
-        if (arr[n - 1][m - 1] == 1) return 0;
-        int[][] dp = new int[n][m]; // 0,0 se n-1,m-1 jane ke paths
+        int n = arr.length;
+        int m = arr[0].length;
+        int[][] dp = new int[n][m];
         for (int i = n - 1; i >= 0; i--) {
             for (int j = m - 1; j >= 0; j--) {
-                //we are the last cell
+                //if i am at the last cell
                 if (i == n - 1 && j == m - 1) {
-                    dp[i][j] = 1;
+                    if (arr[i][j] == 0) dp[i][j] = 1;
                 }
-                // if we are at the last column
-                else if (j == m - 1 && arr[i][j] != 1) {
-                    dp[i][j] = dp[i + 1][j];
-                }
-                //  if we are at the last row
-                else if (i == n - 1 && arr[i][j] != 1) {
-                    dp[i][j] = dp[i][j + 1];
-                }
-                // else where
-
-                else {
-                    if (arr[i][j] != 1) dp[i][j] = dp[i][j + 1] + dp[i + 1][j];
+                //if i am at the last column
+                else if (j == m - 1) {
+                    if (arr[i][j] == 0) dp[i][j] = dp[i + 1][j];
+                } else if (i == n - 1) {
+                    if (arr[i][j] == 0) dp[i][j] = dp[i][j + 1];
+                } else {
+                    if (arr[i][j] == 0) dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
                 }
             }
         }
-        // for(int i=0;i<n;i++)System.out.println(Arrays.toString(dp[i]));
         return dp[0][0];
     }
 }
