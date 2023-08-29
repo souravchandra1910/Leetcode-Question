ff7c1af466a0acde03fbbb0b52a91df93560406d
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2023-08-29 10:05:35 +0530

    Time: 27 ms (94.77%), Space: 50.7 MB (68.64%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -3,27 +3,30 @@ class Solution {
     public List<List<Integer>> threeSum(int[] arr) {
         int n = arr.length;
         Arrays.sort(arr);
+        // System.out.println(Arrays.toString(arr));
         List<List<Integer>> ans = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            if (i > 0 && arr[i] == arr[i - 1]) continue;
-            int j = i + 1;
-            int k = n - 1;
-            while (j < k) {
-                int sum = arr[i] + arr[j] + arr[k];
-                if (sum > 0) {
-                    k--;
-                } else if (sum < 0) {
-                    j++;
-                } else {
-                    List<Integer> al = new ArrayList<>();
-                    al.add(arr[i]);
-                    al.add(arr[j]);
-                    al.add(arr[k]);
-                    ans.add(al);
-                    j++;
-                    k--;
-                    while (j < k && arr[j] == arr[j - 1]) j++;
-                    while (j < k && arr[k] == arr[k + 1]) k--;
+            if (i > 0 && arr[i] == arr[i - 1]) continue; else {
+                int j = i + 1;
+                int k = n - 1;
+                // my i will be a constant and j and k will be acting as two pointers
+                while (j < k) {
+                    int sum = arr[i] + arr[j] + arr[k];
+                    if (sum < 0) {
+                        j++;
+                    } else if (sum > 0) {
+                        k--;
+                    } else {
+                        List<Integer> al = new ArrayList<>();
+                        al.add(arr[i]);
+                        al.add(arr[j]);
+                        al.add(arr[k]);
+                        ans.add(al);
+                        j++;
+                        k--;
+                        while (j < k && arr[j] == arr[j - 1]) j++;
+                        while (j < k && arr[k] == arr[k + 1]) k--;
+                    }
                 }
             }
         }
