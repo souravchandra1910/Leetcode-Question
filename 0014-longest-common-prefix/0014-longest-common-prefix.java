32aea25f339902cad823ed13508b4294c06699df
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2023-09-08 17:33:20 +0530

    Time: 1 ms (82.94%), Space: 40.4 MB (52.63%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,16 +1,16 @@
 class Solution {
 
     public String longestCommonPrefix(String[] str) {
-        if (str.length == 0) return "";
-        if (str.length == 1) return str[0];
-        StringBuilder sb = new StringBuilder();
-        Arrays.sort(str);
-        int len = str[0].length();
-        int i = 0;
-        System.out.println(Arrays.toString(str));
-        for (; i < len; i++) {
-            if (str[0].charAt(i) != str[str.length - 1].charAt(i)) break;
+       if(str.length==0)return "";
+       if(str.length==1)return str[0];
+       Arrays.sort(str);
+      //why sorting?
+     // it is only required to check lexographically largest and smallest strings
+        int len=str[0].length();
+        int i=0;
+        for(;i<len;i++){
+            if(str[0].charAt(i)!=str[str.length-1].charAt(i))break;
         }
-        return str[str.length - 1].substring(0, i);
+        return str[0].substring(0,i);
     }
 }
