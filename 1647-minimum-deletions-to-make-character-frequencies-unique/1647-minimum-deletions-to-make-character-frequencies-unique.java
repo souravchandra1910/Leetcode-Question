1f65c01d1a1c661174902bef9a267a4602292c76
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2023-09-12 08:37:53 +0530

    Time: 8 ms (82.34%), Space: 44.8 MB (13.38%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,31 +1,27 @@
 class Solution {
 
     public int minDeletions(String s) {
-        HashMap<Character, Integer> hm = new HashMap<>();
+        int[] map = new int[26];
         char[] arr = s.toCharArray();
         for (int i = 0; i < arr.length; i++) {
-            hm.put(arr[i], hm.getOrDefault(arr[i], 0) + 1);
+            map[arr[i] - 'a']++;
         }
-        List<Integer> al = new ArrayList<>(hm.values());
-        Collections.sort(al);
-        // System.out.println(al);
-
+        Arrays.sort(map); // 0(26log26)
+        // System.out.println(Arrays.toString(map));
         Set<Integer> set = new HashSet<>();
-
-        set.add(al.get(al.size() - 1));
+        set.add(map[25]);
         int cnt = 0;
-        for (int i = al.size() - 2; i >= 0; i--) {
-            int val = al.get(i);
+        for (int i = 24; i >= 0; i--) {
+            if (map[i] == 0) break;
+            int val = map[i];
             if (set.contains(val) == true) {
-                while (set.contains(val) == true) {
+                while (set.contains(val) == true && val > 0) {
                     val--;
                     cnt++;
-                     if(val==0){
-                        break;
-                    }
                 }
+                if (val == 0) continue;
             }
-            if(val!=0)set.add(val);
+            set.add(val);
         }
         return cnt;
     }
