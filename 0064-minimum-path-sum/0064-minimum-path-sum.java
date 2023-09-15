d1b99950316fa6d0798d1857061829015fbbb35c
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2023-09-15 09:48:54 +0530

    Time: 4 ms (21.61%), Space: 43.6 MB (80.39%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -6,21 +6,14 @@ class Solution {
         int[][] dp = new int[n][m];
         for (int i = n - 1; i >= 0; i--) {
             for (int j = m - 1; j >= 0; j--) {
-                //we are in last cell
                 if (i == n - 1 && j == m - 1) {
                     dp[i][j] = arr[i][j];
-                }
-                // we are in the last column
-                else if (j == m - 1) {
-                    dp[i][j] = arr[i][j] + dp[i + 1][j];
-                }
-                // we are last row;
-                else if (i == n - 1) {
-                    dp[i][j] = arr[i][j] + dp[i][j + 1];
-                }
-                // else where
-                else {
-                    dp[i][j] = Math.min(dp[i + 1][j], dp[i][j + 1]) + arr[i][j];
+                } else if (i == n - 1) {
+                    dp[i][j] = dp[i][j + 1] + arr[i][j];
+                } else if (j == m - 1) {
+                    dp[i][j] = dp[i + 1][j] + arr[i][j];
+                } else {
+                    dp[i][j] = Math.min(dp[i][j + 1], dp[i + 1][j]) + arr[i][j];
                 }
             }
         }
