d509824076e978b50bed704cc8856a4780b74905
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2023-10-05 08:37:02 +0530

    Time: 2 ms (98.52%), Space: 46.7 MB (47.18%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,27 +1,31 @@
 class Solution {
 
-    public List<Integer> majorityElement(int[] arr) {
+    public List<Integer> majorityElement(int[] nums) {
         List<Integer> al = new ArrayList<>();
         int el1 = 0, el2 = 0, cnt1 = 0, cnt2 = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (cnt1 == 0 && arr[i]!=el2) {
-                cnt1 = 1;
-                el1 = arr[i];
-            } else if (cnt2 == 0  && arr[i]!=el1) {
-                cnt2 = 1;
-                el2 = arr[i];
-            } else if (el1 == arr[i]) cnt1++; else if (el2 == arr[i]) cnt2++; else {
+        for (int val : nums) {
+            if (el1 == val) {
+                cnt1++;
+            } else if (el2 == val) {
+                cnt2++;
+            } else if (cnt1 == 0) {
+                el1 = val;
+                cnt1++;
+            } else if (cnt2 == 0) {
+                el2 = val;
+                cnt2++;
+            } else {
                 cnt1--;
                 cnt2--;
             }
         }
-        // System.out.println(el1+"  "+el2);
-        cnt1 = cnt2 = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (el1 == arr[i]) cnt1++; else if (el2 == arr[i]) cnt2++;
+        cnt1 = 0;
+        cnt2 = 0;
+        for (int val : nums) {
+            if (el1 == val) cnt1++; else if (el2 == val) cnt2++;
         }
-        if (cnt1 > arr.length / 3) al.add(el1);
-        if (cnt2 > arr.length / 3) al.add(el2);
+        if (cnt1 > nums.length / 3) al.add(el1);
+        if (cnt2 > nums.length / 3) al.add(el2);
         return al;
     }
 }
