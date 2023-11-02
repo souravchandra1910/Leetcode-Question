2a3de4a823fc5d92f17cc3a119a830e76cfc8b66
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2023-11-02 10:24:06 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (37.66%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -3,20 +3,24 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] arr) {
         int n = arr.length;
         int m = arr[0].length;
-        int[][] dp = new int[n][m];
+         int[][] dp = new int[n][m];
         for (int i = n - 1; i >= 0; i--) {
             for (int j = m - 1; j >= 0; j--) {
-                //if i am at the last cell
-                if (i == n - 1 && j == m - 1) {
-                    if (arr[i][j] == 0) dp[i][j] = 1;
+                //last cell
+                if (i == n - 1 && j == m - 1 && arr[i][j]==0) {
+                    dp[i][j] = 1;
                 }
-                //if i am at the last column
-                else if (j == m - 1) {
-                    if (arr[i][j] == 0) dp[i][j] = dp[i + 1][j];
-                } else if (i == n - 1) {
-                    if (arr[i][j] == 0) dp[i][j] = dp[i][j + 1];
-                } else {
-                    if (arr[i][j] == 0) dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
+                // last row
+                else if (i == n - 1 && arr[i][j]==0) {
+                    dp[i][j] = dp[i][j + 1];
+                }
+                // last col
+                else if (j == m - 1 && arr[i][j]==0) {
+                    dp[i][j] = dp[i + 1][j];
+                }
+                //else where
+                else {
+                   if(arr[i][j]==0) dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
                 }
             }
         }
