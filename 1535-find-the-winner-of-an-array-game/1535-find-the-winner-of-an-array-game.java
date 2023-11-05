e89680d4eef876ce735bff2190591b1576044703
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2023-11-05 18:45:36 +0530

    Time: 0 ms (100.00%), Space: 56.5 MB (65.94%) - LeetHub

---
+++ 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.java
@@ -0,0 +1,15 @@
+class Solution {
+
+    public int getWinner(int[] arr, int k) {
+         int cur = arr[0], win = 0;
+        for (int i = 1; i < arr.length; ++i) {
+            if (arr[i] > cur) {
+                cur = arr[i];
+                win = 0;
+            }
+            win++;
+            if (win == k) break;
+        }
+        return cur;
+    }
+}
