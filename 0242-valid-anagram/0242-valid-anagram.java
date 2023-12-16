840dc3ef30ad0a2107761d381fcab456884755a6
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2023-12-16 09:10:42 +0530

    Time: 1 ms (99.86%), Space: 43.1 MB (68.88%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,16 @@
+class Solution {
+
+    public boolean isAnagram(String s, String t) {
+        int[] map = new int[26];
+        for (char ch : s.toCharArray()) {
+            map[ch - 'a']++;
+        }
+        for (char ch : t.toCharArray()) {
+            map[ch - 'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (map[i] != 0) return false;
+        }
+        return true;
+    }
+}
