b02bd23a0badc59282ff6b5a1fce0a75afc54193
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2023-12-16 09:22:02 +0530

    Time: 1 ms (99.44%), Space: 43.4 MB (28.22%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,27 +1,20 @@
 class Solution {
 
-    public int compress(char[] arr) {
+    public int compress(char[] chars) {
         StringBuilder sb = new StringBuilder();
         int cnt = 1;
-        for (int i = 1; i < arr.length; i++) {
-            char ch1 = arr[i];
-            char ch2 = arr[i - 1];
-            if (ch1 == ch2) {
+        for (int i = 1; i < chars.length; i++) {
+            if (chars[i] == chars[i - 1]) {
                 cnt++;
             } else {
-                sb.append(ch2);
-                if (cnt > 1) {
-                    sb.append(cnt);
-                }
+                sb.append(chars[i - 1]);
+                if (cnt > 1) sb.append(cnt);
                 cnt = 1;
             }
         }
-        sb.append(arr[arr.length - 1]);
+        sb.append(chars[chars.length - 1]);
         if (cnt > 1) sb.append(cnt);
-        // System.out.println(sb);
-        for (int i = 0; i < sb.length(); i++) {
-            arr[i] = sb.charAt(i);
-        }
+        for (int i = 0; i < sb.length(); i++) chars[i] = sb.charAt(i);
         return sb.length();
     }
 }
