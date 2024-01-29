cf691e543af2db52db8c23177ae714019811c913
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2024-01-29 16:46:53 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (40.09%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -1,45 +1,39 @@
 class MyQueue {
+    Stack<Integer> st;
+    Stack<Integer> mt;
 
-    /** Initialize your data structure here. */
-      Stack<Integer>st=new Stack<>();
-      Stack<Integer>helper=new Stack<>();
     public MyQueue() {
-        
+        this.st = new Stack<>();
+        this.mt = new Stack<>();
     }
-    
-    /** Push element x to the back of queue. */
+
     public void push(int x) {
-        // how to implement push()
-		// 1st step -> Move all st elements to helper
-		    while(!st.isEmpty()) {
-		    	int val=st.pop();
-		    	helper.push(val);
-		    }
-		 // 2nd step -> push in x in the st 
-		 st.push(x);
-		 // 3rd step -> Move all helper  elements to st
-		 while(!helper.isEmpty()) {
-			 int val=helper.pop();
-			 st.push(val);
-		 }
+        // 1st step -> push all elements from the main stack to the helper stack
+        while (!st.isEmpty()) {
+            int val = st.pop();
+            mt.push(val);
+        }
+        // 2nd step -> push in x in the st
+        st.push(x);
+        // 3rd step -> Move all helper  elements to st
+        while (!mt.isEmpty()) {
+            int val = mt.pop();
+            st.push(val);
+        }
     }
-    
-    /** Removes the element from in front of queue and returns that element. */
+
     public int pop() {
         return st.pop();
     }
-    
-    /** Get the front element. */
+
     public int peek() {
         return st.peek();
     }
-    
-    /** Returns whether the queue is empty. */
+
     public boolean empty() {
-       	return st.isEmpty();
+        return st.size() == 0;
     }
 }
-
 /**
  * Your MyQueue object will be instantiated and called as such:
  * MyQueue obj = new MyQueue();
